64b9e7e045c5c965bcc647ee0aa479700a0a0e3d
kim6359 <kim63592881@example.com>
2023-05-08 20:22:42 +0900

    개쩌는 구구단

---
+++ Problem2 - GUGUDAN/복     사.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int input;
+    
+    scanf("%d", &input);
+
+    /* Problem 2 : GUGUDAN
+     * 입력 받은 수 input(1~9)에 대해 (구구단의)input단을 출력해봅시다.
+     * 
+     * 이 아래에 작성해봅시다.
+     */
+    for (int i = 1; i <= 9 ; i++ )
+    {
+            printf("%d * %d = %d", input, i, input * i); 
+        
+    }
+    return 0;
+}
