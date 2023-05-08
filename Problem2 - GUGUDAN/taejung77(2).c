ff98ef79c8c4ae7f463e99cb5c0e465c821db7a8
taejung77 <oleh13579@naver.com>
2023-05-08 20:17:22 +0900

    Add problem2 solution

---
+++ Problem2 - GUGUDAN/taejung77(2).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int input;
+    
+    scanf("%d", &input);
+
+    /* Problem 2 : GUGUDAN
+     * 입력 받은 수 input(1~9)에 대해 (구구단의)input단을 출력해봅시다.
+     * 
+     * 이 아래에 작성해봅시다.
+     */
+
+
+    
+
+
+
+
+    return 0;
+}
